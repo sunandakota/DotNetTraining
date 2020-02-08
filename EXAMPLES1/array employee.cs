59c164a558dab0811198fe3c2155a212dbf09468
sunandakota <60807213+sunandakota@users.noreply.github.com>
2020-02-08 12:24:25 +0530

    C# EXAMPLES

---
+++ EXAMPLES1/array employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] employee = new string[2] { "sunny", "denny" };
+            for (int i = 0; i < employee.Length; i++)
+                Console.WriteLine("employee = {0}", employee[i]);
+               Console.ReadKey();
+        }
+    }
+}
